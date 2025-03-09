325d64a132927eb597af8a743e7ccd247c4016dc
boundlessomnipotent <dhruv.03v@gmail.com>
2025-03-09 16:16:22 +0530

    nothing

--- Main.cpp
+++ Main.cpp
@@ -143,13 +143,13 @@ void processInput(GLFWwindow* window)
         glfwSetWindowShouldClose(window, true);
 
     if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
-        camera.ProcessKeyboard(FORWARD, deltaTime);
+        camera.ProcessKeyboard(FORWARD, (deltaTime) +0.12 );
     if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
-        camera.ProcessKeyboard(BACKWARD, deltaTime);
+        camera.ProcessKeyboard(BACKWARD, (deltaTime)+0.12);
     if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
-        camera.ProcessKeyboard(LEFT, deltaTime);
+        camera.ProcessKeyboard(LEFT, (deltaTime)+0.12);
     if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
-        camera.ProcessKeyboard(RIGHT, deltaTime);
+        camera.ProcessKeyboard(RIGHT, (deltaTime)+0.12);
 }
 
 // glfw: whenever the window size changed (by OS or user resize) this callback function executes
